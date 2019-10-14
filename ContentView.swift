734957a823538f40e231330e0d29f73527773863
Adam Mazurick <adam@mazurick.com>
2019-10-14 13:25:44 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mndfl
+//
+//  Created by Adam Mazurick on 2019-10-14.
+//  Copyright © 2019 Adam Mazurick. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+         Text("Rooms")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewLayout(.fixed(width: 1366, height: 1024))
+    }
+}
